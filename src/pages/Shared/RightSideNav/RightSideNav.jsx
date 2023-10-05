88e1996fb6f8f8eac1973f0653bd8fe049d2cdcb
Zahid Hasan <zahidhasan140338@gmail.com>
2023-10-06 02:41:59 +0600

    completed module 52

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,41 @@
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import qZone1 from '../../../assets/qZone1.png'
+import qZone2 from '../../../assets/qZone2.png'
+import qZone3 from '../../../assets/qZone3.png'
+
 
 
 const RightSideNav = () => {
-    return (
-        <div>
-            <h2 className="text-2xl">Right Side</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className='p-4 space-y-3 mb-6'>
+        <h2 className="text-3xl">Login With</h2>
+        <button className="btn btn-outline w-full">
+            <FaGoogle></FaGoogle>
+          Google
+        </button>
+        <button className="btn btn-outline w-full">
+            <FaGithub></FaGithub>
+          Github
+        </button>
+      </div>
+
+      <div className='p-4 mb-6'>
+        <h2 className="text-3xl mb-4">Find Us On</h2>
+        <a href="" className='flex p-4 text-lg items-center border rounded-t-lg'><FaFacebook className='mr-3'></FaFacebook><span>Facebook</span></a>
+        <a href="" className='flex p-4 text-lg items-center border border-x'><FaTwitter className='mr-3'></FaTwitter><span>Twitter</span></a>
+        <a href="" className='flex p-4 text-lg items-center border rounded-b-lg'><FaInstagram className='mr-3'></FaInstagram><span>Instagram</span></a>
+      </div>
+
+      <div className='p-4 space-y-3 mb-6'>
+        <h2 className="text-3xl">Q-Zone</h2>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
